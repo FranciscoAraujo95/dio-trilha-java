aba35289a3fbb7b734b8fe8fc453827a381b3e62
Francisco <fhydglan@gmail.com>
2024-08-07 21:35:20 -0300

    Tipos de variáveis 2

--- tipos-variaveis/src/application/TiposVariaveis.java
+++ tipos-variaveis/src/application/TiposVariaveis.java
@@ -4,5 +4,22 @@ public class TiposVariaveis {
 
     public static void main(String[] args) {
 
+        //Alguns tipos primitivos
+
+        String meuNome = "Francisco Araujo";
+
+        double minimumWage = 2500;
+
+        short shortNumber = 1;
+        int ordinaryNumber = shortNumber;
+        short shortNumber2 = (short) ordinaryNumber;
+
+        int numero = 5;
+        numero = 10;
+
+        System.out.println("numero = " + numero);
+
+        final double VALOR_DE_PI = 3.14;
+
     }
 }
